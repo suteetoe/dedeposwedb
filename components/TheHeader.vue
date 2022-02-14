0a7a1e042f4a899c7a53b9e5d223df5488ea1f40
suteetoe <sutee.toe@gmail.com>
2022-02-14 16:25:02 +0700

    first commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,68 @@
+<template>
+    <header>
+        <div class="relative bg-white">
+            <div class="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
+        <div class="flex justify-start lg:w-0 lg:flex-1">
+          <a href="#">
+            <span class="sr-only">Workflow</span>
+            <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
+          </a>
+        </div>
+        <div class="-mr-2 -my-2 md:hidden">
+          <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+            <span class="sr-only">Open menu</span>
+            <!-- Heroicon name: outline/menu -->
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
+        <nav class="hidden md:flex space-x-10">
+          <div class="relative">
+            <!-- Item active: "text-gray-900", Item inactive: "text-gray-500" -->
+            <!-- <button type="button" class="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" aria-expanded="false">
+              <span>Solutions</span>
+              
+            </button> -->
+
+            <!--
+              'Solutions' flyout menu, show/hide based on flyout menu state.
+
+              Entering: "transition ease-out duration-200"
+                From: "opacity-0 translate-y-1"
+                To: "opacity-100 translate-y-0"
+              Leaving: "transition ease-in duration-150"
+                From: "opacity-100 translate-y-0"
+                To: "opacity-0 translate-y-1"
+            -->
+           
+          </div>
+
+          <a href="/" class="text-base font-medium text-gray-500 hover:text-gray-900"> Home </a>
+
+          <a href="privacy" class="text-base font-medium text-gray-500 hover:text-gray-900"> Privacy </a>
+
+          <a href="terms" class="text-base font-medium text-gray-500 hover:text-gray-900"> Terms & Condition </a>
+
+          <!-- <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"> Company </a> -->
+        </nav>
+        <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+          <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"> Sign in </a>
+          <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </a>
+        </div>
+      </div>
+
+      <!--
+        Mobile menu, show/hide based on mobile menu state.
+
+        Entering: "duration-200 ease-out"
+          From: "opacity-0 scale-95"
+          To: "opacity-100 scale-100"
+        Leaving: "duration-100 ease-in"
+          From: "opacity-100 scale-100"
+          To: "opacity-0 scale-95"
+      -->
+   
+        </div>
+    </header>
+</template>
