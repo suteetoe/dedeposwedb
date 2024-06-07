a2fe7e1302b4e822cbef4cd658314006aa4bbd2a
suteetoe <sutee.toe@gmail.com>
2024-06-07 21:17:26 +0700

    .gitignore is now working

--- .output/server/chunks/build/privacy-styles.dNJ2B-u8.mjs
+++
@@ -1,6 +0,0 @@
-const privacy_vue_vue_type_style_index_0_lang = ".prose-sm :where(p):not(:where([class~=not-prose] *)){margin-bottom:1.1428571em;margin-top:1.1428571em}.prose-sm :where(h2):not(:where([class~=not-prose] *)){font-size:1.875rem;font-weight:700;line-height:1.4;margin-bottom:.8em;margin-top:1.6em;scroll-margin-top:1.6em}.prose-sm :where(h3):not(:where([class~=not-prose] *)){font-size:1.5rem;font-weight:700;line-height:1.4;margin-bottom:.8em;margin-top:1.6em;scroll-margin-top:1.6em}.prose-sm :where(h4):not(:where([class~=not-prose] *)){font-size:1.25rem;font-weight:700;line-height:1.4;margin-bottom:.8em;margin-top:1.6em;scroll-margin-top:1.6em}";
-
-const privacyStyles_dNJ2BU8 = [privacy_vue_vue_type_style_index_0_lang];
-
-export { privacyStyles_dNJ2BU8 as default };
-//# sourceMappingURL=privacy-styles.dNJ2B-u8.mjs.map
